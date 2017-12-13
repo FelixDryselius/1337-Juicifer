69d66233768b2b5660ff67fca72fa918864f303b
Farangis Hakimi <34238647+farangiis@users.noreply.github.com>
2017-12-13 10:16:45 +0100

    Google chart
    Nu har jag lag till en staff-view-branch.

--- views/sw_script.js
+++ views/sw_script.js
@@ -28,3 +28,26 @@ function functionTömDiv() {
     var variabelNamn = document.getElementById('testdiv');
     variabelNamn.innerHTML=" ";
 }
+
+type="text/javascript" src="https://www.gstatic.com/charts/loader.js"
+type="text/javascript"
+      google.charts.load("current", {packages:["corechart"]});
+      google.charts.setOnLoadCallback(drawChart);
+      function drawChart() {
+        var data = google.visualization.arrayToDataTable([
+          ['Task', 'Hours per Day'],
+          ['Work',     11],
+          ['Eat',      2],
+          ['Commute',  2],
+          ['Watch TV', 2],
+          ['Sleep',    7]
+        ]);
+
+        var options = {
+          title: 'My Daily Activities',
+          is3D: true,
+        };
+
+        var chart = new google.visualization.PieChart(document.getElementById('piechart_3d'));
+        chart.draw(data, options);
+      }
