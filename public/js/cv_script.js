1afcc8963bdf3ab10d6e8df9d47b691d91a2fe1b
FelixDryselius <FelixDryselius@users.noreply.github.com>
2017-12-18 13:42:51 +0100

    First_fix_with_script

--- public/js/cv_script.js
+++ public/js/cv_script.js
@@ -1,6 +1,57 @@
+'use strict';
+
 var orderDrinksArray = [];
 
-var startPage = new Vue({
+var all_cv = new Vue({
+    el:'#all_cv',
+    mixins: [sharedVueStuff],
+    data: {
+        showStartPage: true,
+        showSizePage: false,
+        showTopBar: false,
+        showSmoothieIngredPage: false,
+        showJuiceIngredPage: false,
+        showIngredPage: false,
+        showCartPage: false
+    },
+    
+   
+    methods: {
+        hideAllTabs: function () {
+            this.showStartPage = false;
+            this.showSizePage = false;
+            this.showTopBar = false;
+            this.showSmoothieIngredPage = false;
+            this.showJuiceIngredPage = false;
+            this.showIngredPage = false;
+            this.showCartPage = false;
+        },
+        showTab: function (tab) {
+            console.log("test");
+            this.hideAllTabs();
+            if (tab === "smoothieIngredPage") {
+                this.showSmoothieIngredPage = true;
+            }
+            else if (tab === "juiceIngredPage") {
+                this.showJuiceIngredPage = true;
+            }
+            else if (tab === "orderHistory") {
+                this.orderHistoryShow = true;
+            }
+            else if (tab === "inventory") {
+                this.inventoryShow = true;
+            }
+            else if (tab === "statistics") {
+                this.statisticsShow = true;
+            }
+
+        }
+    }
+});
+    
+
+
+/*var startPage = new Vue({
     el: '#start',
     data: {
         seen: true
@@ -47,7 +98,7 @@ var cartPage = new Vue({
   data: {
     seen: false
   }
-})
+})*/
 
 var drink = {
     type : "none", 
@@ -61,9 +112,10 @@ function typeItem(type) {
     orderDrinksArray.push(drink);
     console.log(drink.type + " new drink type");
     console.log(orderDrinksArray[0].type + " content array");
-    startPage.seen = false;
-    topbar.seen = false;
-    sizePage.seen = true;
+    all_cv.showStartPage = false;
+    console.log(all_cv.showStartPage);
+    all_cv.showTopbar = false;
+    all_cv.showSizePage = true;
 }
 
 function sizeItem(size) {
@@ -117,3 +169,88 @@ function sendToCart() {
 console.log(drink.type + " innan");
 console.log(drink.size + " innan");
 
+/*ordering.js
+------------------------------*/
+
+/*jslint es5:true, indent: 2 */
+/*global sharedVueStuff, Vue, socket */
+
+
+Vue.component('ingredient', {
+  props: ['item', 'type', 'lang'],
+  template: ' <div class="ingredient">\
+                  <label>\
+                    <button v-on:click="incrementCounter">{{ counter }}</button>\
+                    {{item["ingredient_"+ lang]}} ({{ (type=="smoothie") ? item.vol_smoothie : item.vol_juice }} ml), {{item.selling_price}}:-, {{item.stock}} pcs\
+                  </label>\
+              </div>',
+  data: function () {
+    return {
+      counter: 0
+    };
+  },
+  methods: {
+    incrementCounter: function () {
+      this.counter += 1;
+      this.$emit('increment');
+    },
+    resetCounter: function () {
+      this.counter = 0;
+    }
+  }
+});
+
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min)) + min;
+}
+
+function getOrderNumber() {
+  // It's probably not a good idea to generate a random order number, client-side. 
+  // A better idea would be to let the server decide.
+  return "#" + getRandomInt(1, 1000000);
+}
+
+var vm = new Vue({
+  el: '#all_cv',
+  mixins: [sharedVueStuff], // include stuff that is used both in the ordering system and in the kitchen
+  data: {
+    type: '',
+    chosenIngredients: [],
+    volume: 0,
+    price: 0
+  },
+  methods: {
+    addToOrder: function (item, type) {
+      this.chosenIngredients.push(item);
+      this.type = type;
+      if (type === "smoothie") {
+        this.volume += +item.vol_smoothie;
+      } else if (type === "juice") {
+        this.volume += +item.vol_juice;
+      }
+      this.price += +item.selling_price;
+    },
+    placeOrder: function () {
+      var i,
+      //Wrap the order in an object
+        order = {
+          ingredients: this.chosenIngredients,
+          volume: this.volume,
+          type: this.type,
+          price: this.price
+        };
+      // make use of socket.io's magic to send the stuff to the kitchen via the server (app.js)
+      socket.emit('order', {orderId: getOrderNumber(), order: order});
+      //set all counters to 0. Notice the use of $refs
+      for (i = 0; i < this.$refs.ingredient.length; i += 1) {
+        this.$refs.ingredient[i].resetCounter();
+      }
+      this.volume = 0;
+      this.price = 0;
+      this.type = '';
+      this.chosenIngredients = [];
+    }
+  }
+});
