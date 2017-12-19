8ac3f86894b3b9be29da3015851e76d46680648b
deab90 <dennis_abrahamsson@hotmail.com>
2017-12-19 16:59:28 +0100

    Lade till fil superorder.js med anteckningar
    Antecknaingarna beskriver strukturen för hur en order skall registreras.

---
+++ superorder.js
@@ -0,0 +1,12 @@
+var superorder = [
+    null /*Här placeras orderid när det genererats*/,
+    "drink1",
+    "drink2" /*osv.*/
+]
+
+var drink = {
+    ingredients: [],
+    volume: 0, /*Domän {1,2,3}*/
+    type : "",
+    prize : 0,
+}
