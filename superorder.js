fb68c399d4502e20a1da332367ae9f31dadced23
deab90 <dennis_abrahamsson@hotmail.com>
2017-12-20 10:32:55 +0100

    Uppdaterade superorder.js

--- superorder.js
+++ superorder.js
@@ -2,18 +2,16 @@ var superorder = {
     done: false,            //boolean
     orderId : 1,            //int
     numberOfDrinks : 2,     //int
-    drink1 : {                   //object
-        ingredients: [],    //array med id för ingredienser
-        volume: 0,          //int
-        type : "",          //string
-        prize : 0,          //int
-        aborted: false      //boolean
-    },
-    drink2 : {                   //object
-        ingredients: [],    //array med id för ingredienser
-        volume: 0,          //int
-        type : "",          //string
-        prize : 0,          //int
-        aborted: false      //boolean
-    }
+    drinks : [],            //En array av object
+    orderTime: "enTid"      //Hur skall detta lagras?
+}
+
+
+
+var drink = {           //object
+    ingredients: [],    //array med id för ingredienser
+    volume: 0,          //int
+    type : "",          //string
+    prize : 0,          //int
+    aborted: false      //boolean
 }
