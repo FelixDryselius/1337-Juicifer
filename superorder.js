999076b490c79204cefd3ffbec1c9f31fdebad9f
deab90 <dennis_abrahamsson@hotmail.com>
2017-12-20 10:44:13 +0100

    Update superorder.js

--- superorder.js
+++ superorder.js
@@ -1,9 +1,9 @@
 var superorder = {
-    done: false,            //boolean
-    orderId : 1,            //int
-    numberOfDrinks : 2,     //int
-    drinks : [],            //En array av object
-    orderTime: "enTid"      //Hur skall detta lagras?
+    done: false,            //boolean, håller reda på om drinken skall ligga i orderkö eller historik.
+    orderId : -1,           //int, tilldelas när ordern skickas från cv.
+    drinks : [],            //En array av object som innehåller själva drinkarna.
+    activeDrink : 0         //int. Index för den drink i arrayen drinks som kunden håller på att ändra.
+    orderTime: "enTid"      //Tiden då ordern lades. Hur skall detta lagras?
 }
 
 
