89cfc5544c3fb811bf2ed233c5e1ddedc6255c37
deab90 <dennis_abrahamsson@hotmail.com>
2017-12-26 21:08:01 +0100

    Update cv_script.js

--- public/js/cv_script.js
+++ public/js/cv_script.js
@@ -255,7 +255,7 @@ placeSuperOrder: function () {
 sendCurrentSuperOrderToVue()
 // make use of socket.io's magic to send the stuff to the kitchen via the server (app.js)
 
-socket.emit('SuperOrder', {orderId: getOrderNumber(), superOrderProperties: this.vueSuperOrder});
+socket.emit('superOrder', {orderId: getOrderNumber(), superOrderProperties: this.vueSuperOrder});
 
 //This is for checking that it works
 console.log("skickade superOrder");
